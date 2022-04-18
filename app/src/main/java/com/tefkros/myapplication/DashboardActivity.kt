80db9274c2a483f2b1b82ce6b7a20bf97652a698
Constantinos Loizou <constantinoslo98@gmail.com>
2022-04-18 16:19:04 +0300

    Link dashboard butttons to methods

--- app/src/main/java/com/tefkros/myapplication/DashboardActivity.kt
+++ app/src/main/java/com/tefkros/myapplication/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.tefkros.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.card.MaterialCardView
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,4 +14,22 @@ class DashboardActivity : AppCompatActivity() {
         title = "Dashboard"
 
     }
+
+    fun btnStakeholdersDir(view: android.view.View) {
+        Log.d(LOG_TAG,"Stakeholders Directory Clicked")
+    }
+    fun btnIndividualsDir(view: android.view.View) {
+        Log.d(LOG_TAG,"Individuals Directory Clicked")
+    }
+    fun btnOrganizationsDir(view: android.view.View) {
+        Log.d(LOG_TAG,"Organizations Directory Clicked")
+    }
+    fun btnHepaCourse(view: android.view.View) {
+        Log.d(LOG_TAG,"HEPA4ALL Course Clicked")
+    }
+    fun btnActivitiesDir(view: android.view.View) {
+        Log.d(LOG_TAG,"Activities Directory Clicked")
+    }
+
+
 }
