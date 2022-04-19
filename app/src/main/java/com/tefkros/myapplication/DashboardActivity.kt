496c2d03d63091e80a3492111c71254776c16355
Constantinos Loizou <constantinoslo98@gmail.com>
2022-04-19 13:33:27 +0300

    Basic DrawerLayout functionality implemented

--- app/src/main/java/com/tefkros/myapplication/DashboardActivity.kt
+++ app/src/main/java/com/tefkros/myapplication/DashboardActivity.kt
@@ -1,17 +1,35 @@
 package com.tefkros.myapplication
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.card.MaterialCardView
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var mDrawer: DrawerLayout
+    private lateinit var mToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         Log.d(LOG_TAG, "DashboardActivity loaded")
 
         title = "Dashboard"
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        mDrawer = findViewById(R.id.drawerLayout)
+        mToggle = ActionBarDrawerToggle(this, mDrawer, R.string.open, R.string.close)
+        mDrawer.addDrawerListener(mToggle)
+        mToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
 
@@ -31,5 +49,37 @@ class DashboardActivity : AppCompatActivity() {
         Log.d(LOG_TAG,"Activities Directory Clicked")
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onBackPressed() {
+        // If drawer is open just close it
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START)
+        } else {
+        // If drawer is closed show app exit dialog
+            val builder = AlertDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.exit_promt)
+                .setCancelable(false)
+                .setPositiveButton("Yes") { dialog, id -> finish() }
+                .setNegativeButton("No") { dialog, id -> dialog.cancel() }
+            builder.create().show()
+        }
+    }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        mToggle.syncState()
+    }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mToggle.onConfigurationChanged(newConfig)
+    }
 
 }
