87386950f04c7a523180f784d357752e8a02102f
Constantinos Loizou <constantinoslo98@gmail.com>
2022-05-02 16:50:53 +0300

    Add progressbar and disable login button while loggin in

--- app/src/main/java/com/tefkros/myapplication/LoginActivity.kt
+++ app/src/main/java/com/tefkros/myapplication/LoginActivity.kt
@@ -4,24 +4,52 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mProgressBar : ProgressBar
+    private lateinit var mBtnLogin : MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d(LOG_TAG, "LoginActivity loaded")
+
+        mProgressBar = findViewById(R.id.progressBar)
+        mBtnLogin = findViewById(R.id.btnLogin)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Initially the progressbar is invisible
+        mProgressBar.visibility = View.INVISIBLE
+
+        // Enable login button if it's disabled
+       mBtnLogin.isEnabled = true
+
     }
 
     fun login(view: android.view.View) {
         Log.d(LOG_TAG, "Login button pressed")
+        mProgressBar.visibility = View.VISIBLE
+
+        // Disable login button while login is in process
+        mBtnLogin.isEnabled = false
         // Implement firebase login
+
+        // Skip formal login (for testing only)
+        guestLogin()
+
     }
 
     fun signup(view: android.view.View) {
         Log.d(LOG_TAG, "Signup button pressed")
         // implement firebase signup
     }
-    fun guestLogin(view: android.view.View) {
+    fun guestLogin() {
         Log.d(LOG_TAG, "Guest login button pressed")
         val intent = Intent(this, DashboardActivity::class.java)
         startActivity(intent)
